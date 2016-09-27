f0337d28090fb3cd7566fa0ad21d1f4b9fe820ee
MadokaFujimaki <madoka.fujimaki@me.com>
2016-09-27 20:23:52 +0200

    Removed unused items

--- Uppgift2_Shotgun/Uppgift2_Shotgun/FightControl.cs
+++ Uppgift2_Shotgun/Uppgift2_Shotgun/FightControl.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Diagnostics.Eventing.Reader;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace Uppgift2_Shotgun
--- Uppgift2_Shotgun/Uppgift2_Shotgun/Functions/ImageControl.cs
+++ Uppgift2_Shotgun/Uppgift2_Shotgun/Functions/ImageControl.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Configuration;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Uppgift2_Shotgun
 {
